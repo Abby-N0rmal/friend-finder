ab2b589b288e68ce0bd1c6ef3b3135f95dad3bf9
Amber Girard <rhadorasrequiem@gmail.com>
2018-04-23 18:22:59 -0400

    final stages

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,21 +2,124 @@
 //initialize starting with 'dummy' data
 var friendList = [
   {
-    name: 'Amber Girard',
-    profilePic: 'https://avatars1.githubusercontent.com/u/33493282?s=400&u=e42790244ba7341f34bf660edd3f5e57e929679b&v=4',
+    name: 'Morty',
+    profilePic: "https://vignette.wikia.nocookie.net/rickandmorty/images/b/bc/Vlcsnap-2015-01-31-04h27m25s140.png/revision/latest?cb=20150131122752",
+    scores: [
+      "3",
+      "1",
+      "1",
+      "1",
+      "4",
+      "3",
+      "4",
+      "2",
+      "4",
+      "4"
+    ]
+  },
+  
+  {
+    name: 'Rick',
+    profilePic: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+    scores: [
+      "3",
+      "5",
+      "1",
+      "5",
+      "5",
+      "5",
+      "1",
+      "5",
+      "1",
+      "5"
+    ]
+  },
+  
+  {
+    name: 'Summer',
+    profilePic: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest/scale-to-width-down/310?cb=20160919183158",
+    scores: [
+      "1",
+      "1",
+      "5",
+      "2",
+      "4",
+      "3",
+      "5",
+      "3",
+      "4",
+      "3"
+    ]
+  },
+  
+  {
+    name: 'Beth',
+    profilePic: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a2/S1e11_beth_smith.png/revision/latest?cb=20160919003505",
+    scores: [
+      "5",
+      "5",
+      "3",
+      "4",
+      "3",
+      "3",
+      "5",
+      "4",
+      "5",
+      "3"
+    ]
+  },
+ 
+  {
+    name: 'Jerry',
+    profilePic: "https://vignette.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.png/revision/latest?cb=20160923151111",
     scores: [
       "2",
+      "1",
+      "4",
+      "1",
+      "1",
+      "1",
+      "5",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+
+    {
+    name: 'Jessica',
+    profilePic: "https://vignette.wikia.nocookie.net/rickandmorty/images/e/e8/Jessica.png/revision/latest?cb=20150706222334",
+    scores: [
       "5",
       "3",
       "5",
+      "3",
+      "4",
+      "4",
       "5",
+      "4",
       "5",
-      "5",
+      "4"
+    ]
+  },
+
+      {
+    name: 'Mr. Meeseeks',
+    profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkDYfe7UgocF_t-eUNN0Atawpn9CdwB0j8EcGGTZxb0i0zSZvGAQ",
+    scores: [
+      "4",
       "2",
+      "2",
+      "3",
       "5",
-      "5"
+      "3",
+      "5",
+      "4",
+      "5",
+      "2"
     ]
   }
 ]
+
 
 module.exports = friendList;

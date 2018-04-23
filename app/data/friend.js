ad69e51f92fdc3fab14b81c6fa6d1693607f51b5
Amber Girard <rhadorasrequiem@gmail.com>
2018-04-23 12:11:00 -0400

    duplicate question fixed, fixed dummy data too

--- app/data/friend.js
+++ app/data/friend.js
@@ -5,16 +5,16 @@ var friendList = [
     name: 'Amber Girard',
     profilePic: 'https://avatars1.githubusercontent.com/u/33493282?s=400&u=e42790244ba7341f34bf660edd3f5e57e929679b&v=4',
     scores: [
-      "4",
+      "2",
+      "5",
+      "3",
+      "5",
+      "5",
       "5",
       "5",
       "2",
-      "4",
       "5",
-      "4",
-      "4",
-      "3",
-      "4"
+      "5"
     ]
   }
 ]
